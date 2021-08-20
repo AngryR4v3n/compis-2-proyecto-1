d658c9cd73feb243a2b132baa6ed17f4cd6e58d2
Francisco Molina <molinajimenez01@gmail.com>
2021-08-20 00:56:27 -0600

    initial symbol table with some scope implemented

--- AnPy.py
+++ AnPy.py
@@ -1,43 +1,27 @@
-from DecafListener import Overrider
 import sys
-from antlr4 import *
+from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
 from DecafLexer import DecafLexer
 from DecafParser import DecafParser
- 
-"""
-Symbol Table:
-{Entry.uid: {...} }
-"""
-class SymbolTable():
-    def __init__(self):
-        self.table = {}
-    def add_entry():
-        pass
-
-"""
-Single Entry...
-{uid: 13414141, varType: "int", name: "num", value: "1" scope: "global"}
-"""
-class SymbolTableEntry():
-    def __init__(self, uid, varType, name, value, symbolType, scope="global"):
-        self.uid = hash(uid)
-        self.varType = varType
-        self.name = name
-        self.scope = scope
-        self.value = value
-        self.symbolType  = symbolType
+from CustomListeners import CustomListener
 
 
 
 def main(argv):
-    input_stream = FileStream(argv[1])
+    input_stream = FileStream("./tests/fact_array.txt")
     lexer = DecafLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = DecafParser(stream)
     tree = parser.program()
-    printer = Overrider()
     walker = ParseTreeWalker()
-    walker.walk(printer, tree)
- 
+    Customlisteners = CustomListener()
+    walker.walk(Customlisteners, tree)
+
+
+def traverse(tree):
+    if tree.getText() == "<EOF>":
+        return None
+    else:
+        print(tree.getText())
+
 if __name__ == '__main__':
     main(sys.argv)
--- CustomListeners.py
+++ CustomListeners.py
@@ -1,4 +1,110 @@
 from DecafListener import DecafListener
+from DecafParser import DecafParser
+from antlr4 import TerminalNode
+
+
+"""
+Symbol Table:
+{Entry.name: {...} }
+"""
+
+
+class SymbolTable():
+    def __init__(self):
+        self.table = {"global": {}}
+        self.scope = "global"
+
+    # Hay que hacer validaciones no?
+    def add_entry(self, entry):
+
+        obj = {
+            entry.name: {
+                "uid": entry.uid,
+                "varType": entry.varType, 
+                "name": entry.name, 
+                "value": entry.value
+            }
+        }
+
+        try:
+            prevValues = {**self.table[self.scope]}
+            print("updating scope")
+            add = prevValues | obj
+            self.table[self.scope] = prevValues | obj
+
+        except KeyError:
+            print("Unexisting scope")
+            self.table[self.scope] = { 
+                entry.name:{
+                    "uid": entry.uid, 
+                    "varType": entry.varType, 
+                    "value": entry.value
+                }
+            }
+
+        print(self.table)
+
+
+"""
+Single Entry...
+{uid: 13414141, varType: "int", name: "num", value: "1" scope: "global"}
+"""
+
+
+class SymbolTableEntry():
+    def __init__(self, uid, varType, name, value, symbolType, scope="global"):
+        self.uid = hash(uid)
+        self.varType = varType
+        self.name = name
+        self.scope = scope
+        self.value = value
+        self.symbolType = symbolType
+
+
 class CustomListener(DecafListener):
     def __init__(self) -> None:
+        self.table = SymbolTable()
         super().__init__()
+
+    def typeValidation(self, varType, varValue):
+        pass
+
+    def textExtractor(self, toExtract):
+        if toExtract:
+            if isinstance(toExtract, TerminalNode):
+                return toExtract.symbol.text
+            else:
+                toExtract = toExtract.getChild(0)
+                final = self.textExtractor(toExtract)
+                return final
+        else:
+            return None
+
+    def enterVarDeclaration(self, ctx: DecafParser.VarDeclarationContext):
+        varType = self.textExtractor(ctx.getChild(0))
+        varName = self.textExtractor(ctx.getChild(1))
+
+        # si se declaro un int a[numero]
+        num = self.textExtractor(ctx.NUM())
+        if num:
+            if int(num) < 0:
+                raise Exception("IndexError: Please provide a valid number")
+
+        if len(ctx.children) > 2:
+            print("Arr declaration!")
+            entry = SymbolTableEntry(
+                varName, varType, varName, "Array", "var", scope=self.table.scope)
+        else:
+            varValue = ctx.getChild(2)
+            entry = SymbolTableEntry(
+                varName, varType, varName, varValue, "var", scope=self.table.scope)
+
+        self.table.add_entry(entry)
+
+        return super().enterVarDeclaration(ctx)
+
+    def enterMethodDeclaration(self, ctx: DecafParser.MethodDeclarationContext):
+        methodReturnType = self.textExtractor(ctx.getChild(0))
+        methodName = self.textExtractor(ctx.getChild(1))
+        self.table.scope = methodName
+        return super().enterMethodDeclaration(ctx)
